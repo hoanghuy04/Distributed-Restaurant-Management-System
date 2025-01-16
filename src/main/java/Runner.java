b350123d8022b0976601eda7645e91c437ea189d
DuongHoangHuy <hoangyeu365@gmail.com>
2025-01-16 21:30:19 +0700

    Set up project

---
+++ src/main/java/Runner.java
@@ -0,0 +1,14 @@
+/*
+ * @ (#) Runner.java      1.0      1/16/2025
+ *
+ * Copyright (c) 2025 IUH. ALL rights reserved.
+ */
+
+/*
+ * @description:
+ * @author: Hoang Huy
+ * @date: 1/16/2025
+ * @version: 1.0
+ */
+public class Runner {
+}
