9dab09180d3dbd31632ee0847649debc01185c77
TranNgocHuyen19 <tranngochuyenn1909@gmail.com>
2025-01-18 21:03:28 +0700

    feat(utils): add utility for generating mock data

---
+++ src/main/java/util/datafaker/DataGenerator.java
@@ -0,0 +1,114 @@
+package util.datafaker;
+
+import dal.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import model.*;
+import net.datafaker.Faker;
+
+import java.util.Random;
+
+/*
+ * @description: DataGenerator
+ * @author: Trần Ngọc Huyền
+ * @date: 1/18/2025
+ * @version: 1.0
+ */
+public class DataGenerator {
+
+    Faker faker = new Faker(new java.util.Locale("vi"));
+    private final Random rand = new Random();
+    EntityManager em = Persistence.createEntityManagerFactory("mariadb").createEntityManager();
+    private final CategoryDAL categoryDAL = new CategoryDAL(em);
+    private final ItemDAL itemDAL = new ItemDAL(em);
+    private final ToppingDAL toppingDAL = new ToppingDAL(em);
+    private final ItemToppingDAL itemToppingDAL = new ItemToppingDAL(em);
+//    private final EmployeeDAL employeeDAL = new EmployeeDAL(em);
+    private final RoleDAL roleDAL = new RoleDAL();
+//    private final PromotionDAL promotionDAL = new PromotionDAL(em);
+//    private final PromotionDetailDAL promotionDetailDAL = new PromotionDetailDAL(em);
+//    private final CustomerDAL customerDAL = new CustomerDAL(em);
+//    private final FloorDAL floorDAL = new FloorDAL(em);
+//    private final TableDAL tableDAL = new TableDAL(em);
+//    private final OrderDAL orderDAL = new OrderDAL(em);
+    private final OrderDetailDAL orderDetailDAL = new OrderDetailDAL(em);
+
+    // CategoryEntity
+    private CategoryEntity generateCategoryEntity() {
+        return null;
+    }
+
+    // ItemEntity
+    private ItemEntity generateItemEntity() {
+        return null;
+    }
+
+    // ToppingEntity
+    private ToppingEntity generateToppingEntity() {
+        return null;
+    }
+
+    // ItemToppingEntity
+    private ItemToppingEntity generateItemToppingEntity() {
+        return null;
+    }
+
+    //Address
+    private Address generateAddress() {
+        return null;
+    }
+
+    // EmployeEntity
+    private EmployeeEntity generateEmployeeEntity() {
+        return null;
+    }
+
+    // RoleEntity
+    private RoleEntity generateRoleEntity() {
+        return null;
+    }
+
+    // PromotionEntity
+    private PromotionEntity generatePromotionEntity() {
+        return null;
+    }
+
+    // PromotionDetailEntity
+    private PromotionDetailEntity generatePromotionDetailEntity() {
+        return null;
+    }
+
+    // CustomerEntity
+    private CustomerEntity getCustomerEntity() {
+        return null;
+    }
+
+    // FloorEntity
+    private FloorEntity generateFloorEntity() {
+        return null;
+    }
+
+    //TableEntity
+    private TableEntity getTableEntity() {
+        return null;
+    }
+
+    //OrderEntity
+    private OrderEntity generateOrderEntity() {
+        return null;
+    }
+
+    public void generateAndPrintSampleData() {
+        EntityTransaction tr = em.getTransaction();
+
+        for (int i = 0; i < 10; i++) {
+            //new Entity
+            tr.begin();
+            //persit
+            tr.commit();
+        }
+    }
+
+
+}
