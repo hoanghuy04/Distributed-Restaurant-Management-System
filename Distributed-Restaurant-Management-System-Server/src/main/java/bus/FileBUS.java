48edd752834ca35bde41e061f705c92f191b4dfa
HoangOnGIT <22652761.hoang@student.iuh.edu.vn>
2025-04-27 16:07:24 +0700

    chore: CRUD item validated

--- Distributed-Restaurant-Management-System-Server/src/main/java/bus/FileBUS.java
+++ Distributed-Restaurant-Management-System-Server/src/main/java/bus/FileBUS.java
@@ -6,6 +6,5 @@ import java.rmi.RemoteException;
 public interface FileBUS extends Remote {
     public void uploadFileToServer(byte[] mybyte, String serverpath, int length) throws RemoteException;
     public byte[] downloadFileFromServer(String servername) throws RemoteException;
-
     public String[] listFiles(String serverpath) throws RemoteException;
 }
